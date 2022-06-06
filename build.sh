28307cda619cf2201b69b4cd1d144786b262a296
Atridad Lahiji <88056492+atridadl@users.noreply.github.com>
2022-06-06 16:11:07 -0600

    Update to HAS 2022.6.2

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ cd /app/data
 virtualenv -p /usr/bin/python3.9 hass
 source /app/data/hass/bin/activate
 pip3 install wheel
-pip3 install --no-use-pep517 homeassistant==2022.3.7
+pip3 install --no-use-pep517 homeassistant==2022.6.2
 # python3 /app/data/hass/lib/python3.8/site-packages/homeassistant/setup.py
